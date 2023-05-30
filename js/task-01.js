3834e8227c5ec8778f076e25a412870792141682
Andrzej Borzym <borzym.a@gmail.com>
2023-05-30 09:05:07 +0200

    task,01

---
+++ js/task-01.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const lists = document.querySelectorAll(".item");
+console.log(lists);
+
+const categories = document.querySelectorAll("h2");
+console.log(categories);
+
+console.log(`Number of categories: ${lists.length}`);
+
+console.log(categories[0].textContent);
+
+console.log;
