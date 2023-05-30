7eab876ef1cac6ff7255f4b14190945af8c20d62
aborzym <borzym.a@gmail.com>
2023-05-30 11:16:41 +0200

    task 02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const list = document.querySelector("#ingredients");
+console.log(list);
+ingredients.forEach((el) => {
+  const listElement = document.createElement("li");
+  listElement.textContent = el;
+  listElement.classList.add("item");
+  list.append(listElement);
+});
