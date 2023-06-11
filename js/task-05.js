d4b4898043d4fe28ea58856515bfbccb0bcaf394
aborzym <borzym.a@gmail.com>
2023-06-11 14:30:51 +0200

    task 05

--- js/task-05.js
+++ js/task-05.js
@@ -1,1 +1,9 @@
 "use strict";
+const inputName = document.getElementById("name-input");
+const outputName = document.getElementById("name-output");
+const f = () => {
+  if (inputName.value === "") outputName.textContent = "Anonyomus";
+  else outputName.textContent = inputName.value;
+};
+
+input.addEventListener("input", f);
