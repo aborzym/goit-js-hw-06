50b5062321305d581d29687f12255439714af09a
aborzym <borzym.a@gmail.com>
2023-06-12 09:22:15 +0200

    task 05 done

--- js/task-05.js
+++ js/task-05.js
@@ -1,9 +1,9 @@
 "use strict";
 const inputName = document.getElementById("name-input");
 const outputName = document.getElementById("name-output");
-const f = () => {
-  if (inputName.value === "") outputName.textContent = "Anonyomus";
+const greetingName = () => {
+  if (inputName.value === "") outputName.textContent = "Anonymous";
   else outputName.textContent = inputName.value;
 };
 
-input.addEventListener("input", f);
+inputName.addEventListener("input", greetingName);
