edad401fe36e0fb97f4287c6908d21673ded5b71
aborzym <borzym.a@gmail.com>
2023-06-12 10:33:15 +0200

    task 09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, "0")}`;
 }
+
+const colorValue = document.querySelector(".color");
+const changeColorButton = document.querySelector(".change-color");
+
+const changeColor = () => {
+  // wygenerować kolor
+  const randomColor = getRandomHexColor();
+  //zmienić styl body
+  document.body.style.backgroundColor = randomColor;
+  //wypisać kod koloru
+  colorValue.textContent = randomColor;
+};
+
+changeColorButton.addEventListener("click", changeColor);
