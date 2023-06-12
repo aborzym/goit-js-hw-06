08b160d9a8f70c2e258d758af4dca5d8c982c5e7
aborzym <borzym.a@gmail.com>
2023-06-12 10:16:06 +0200

    task 08 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+"use strict";
 
+const loginForm = document.querySelector(".login-form");
+
+const validate = (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  //sprawdzanie czy pola nie są niewypełnione
+
+  if (email.value === "" || password.value === "")
+    alert("Wszystkie pola powinny być wypełnione!");
+  else {
+    const loginData = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(loginData);
+    loginForm.reset();
+  }
+};
+
+loginForm.addEventListener("submit", validate);
