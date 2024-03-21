8ee57c4da0e63012ff62ec88d6274bfd0a07be10
aborzym <borzym.a@gmail.com>
2024-03-21 12:24:55 +0100

    task 02 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
+"use strict";
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+for (const ingredient of ingredients) {
+  const listElement = document.createElement("li");
+  listElement.textContent = ingredient;
+  listElement.classList.add("item");
+  list.append(listElement);
+}
