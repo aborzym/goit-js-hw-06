4df7c69bfddb16442ce55e9432d2474aff91b0a2
aborzym <borzym.a@gmail.com>
2024-03-23 15:36:31 +0100

    task 08 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,13 @@
-
+const form = document.querySelector("form");
+form.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+  const {
+    elements: { email, password },
+  } = ev.target;
+  if (email.value === "" || password.value === "") {
+    window.alert("Wszystkie pola powinny zostać wypełnione");
+  } else {
+    console.log({ email: email.value, password: password.value });
+    form.reset();
+  }
+});
