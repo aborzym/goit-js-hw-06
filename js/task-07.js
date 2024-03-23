83960f6b4eaff47acb3c8452d4f1f199973f7d62
aborzym <borzym.a@gmail.com>
2024-03-23 09:36:56 +0100

    task 06 done

---
+++ js/task-07.js
@@ -0,0 +1,7 @@
+const slider = document.querySelector("#font-size-control");
+const text = document.querySelector("#text");
+
+slider.addEventListener("input", (ev) => {
+  const newSize = `${ev.currentTarget.value}px`;
+  text.style.fontSize = newSize;
+});
