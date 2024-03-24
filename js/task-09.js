b32cc168de5c604ea459a584aa2b98cbf18914f2
aborzym <borzym.a@gmail.com>
2024-03-24 01:46:19 +0100

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const body = document.querySelector("body");
+
+const colorDisplay = document.querySelector(".color");
+const changeBtn = document.querySelector(".change-color");
+
+changeBtn.addEventListener("click", (ev) => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  colorDisplay.textContent = randomColor;
+});
