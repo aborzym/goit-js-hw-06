1467ef6ac1640ac350984f0bfa963aa2f951b63c
aborzym <borzym.a@gmail.com>
2024-03-25 09:22:20 +0100

    updated task 07

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,6 @@
 const slider = document.querySelector("#font-size-control");
-
 const text = document.querySelector("#text");
-
+text.style.fontSize = `${slider.value}px`;
 slider.addEventListener("input", (ev) => {
   const newSize = `${ev.currentTarget.value}px`;
   text.style.fontSize = newSize;
